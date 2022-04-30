8a8e0a94729ffbf3895cf370633547e4b57cf8d5
Minori Akizuki <dreamnstd@gmail.com>
2022-05-01 04:58:07 +0900

    双椪のテストケースを追加。

--- tests/unit/galaxy_rule/solve_hule_tile.spec.ts
+++ tests/unit/galaxy_rule/solve_hule_tile.spec.ts
@@ -15,4 +15,15 @@ describe('通常形上がり牌探索', () => {
         waitForm[_waitForm])
     })
   })
+
+  it('双椪判定', () => {
+    const hand = parser('nnll')
+    const wait = GALAXY_RULE.solveHuleTileCommoon(hand)
+    wait.forEach(([mianzis, waitTile, _waitForm]) => {
+      console.log(
+        mianzis.map(m => galaxyMianziToString(m)).join(','),
+        waitTile.map(t => t.toString()).join(','),
+        waitForm[_waitForm])
+    })
+  })
 })
