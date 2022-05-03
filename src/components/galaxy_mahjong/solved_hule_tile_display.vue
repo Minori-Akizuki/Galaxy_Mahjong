e7fd0bd1e91d9f4bb17fb73dd0be1ed0d102c333
Minori Akizuki <dreamnstd@gmail.com>
2022-05-04 06:28:59 +0900

    上がりパターン絞りこみ機能の追加

--- src/components/galaxy_mahjong/solved_hule_tile_display.vue
+++ src/components/galaxy_mahjong/solved_hule_tile_display.vue
@@ -3,20 +3,25 @@
     <div class="row">
       <div class="col">
         上がり牌 :
-        <tile-image v-for="tile in waitTiles" :key="tile[1]" :tile="tile[0]" small/>
+        <tile-image v-for="tile in waitTiles" :key="tile[1]" :tile="tile[0]" small v-on:click="turnOnHuleTileFilter(tile[0])" />
+        <button type="button" class="btn btn-outline-info btn-sm" v-on:click="turnOffHuleTileFilter">
+          {{filterButtonMessage}}
+        </button>
       </div>
     </div>
     <hr />
     <div>
       <div v-for="hulePattern in solvedHuleTiles" :key="hulePattern[3]">
-        <div class="row row-cols-auto">
-          <display-mianzi v-for="(mianzi, idx) in hulePattern[0]" :key="idx" :mianzi="() => mianzi" small showColor class="col"/>
-          <span class="col">
-            <tile-image v-for="(tile, idxT) in hulePattern[1]" :key="idxT" :tile="tile" small />
-            {{ formStrings[hulePattern[2]] }}
-          </span>
+        <div v-if="!filterMode || isContainedHuleTile(hulePattern, filterTile)">
+          <div class="row row-cols-auto">
+            <display-mianzi v-for="(mianzi, idx) in hulePattern[0]" :key="idx" :mianzi="() => mianzi" small showColor class="col"/>
+            <span class="col">
+              <tile-image v-for="(tile, idxT) in hulePattern[1]" :key="idxT" :tile="tile" small />
+              {{ formStrings[hulePattern[2]] }}
+            </span>
+          </div>
+          <hr />
         </div>
-        <hr />
       </div>
     </div>
   </div>
@@ -45,9 +50,28 @@ export default class SolvedHuleTileDisplay extends Vue {
   uid = 0
   waitUniqTiles!:[MahjongTile, number][]
   formStrings = ['両面', '嵌張', '辺張', '単騎', '双椪', 'その他']
+  filterMode = false
+  filterButtonMessage = '牌タップで絞りこみ'
+  filterTile!:MahjongTile
   created ():void {
     this.store = useStore()
     this.waitUniqTiles = []
+  }
+
+  public turnOnHuleTileFilter (tile:MahjongTile):void {
+    this.filterMode = false
+    this.filterTile = tile
+    this.filterMode = true
+    this.filterButtonMessage = 'フィルター解除'
+  }
+
+  public turnOffHuleTileFilter ():void {
+    this.filterMode = false
+    this.filterButtonMessage = '牌タップで絞りこみ'
+  }
+
+  public isContainedHuleTile (pattern:[IMianzi[], MahjongTile[], waitForm, number], tile:MahjongTile):boolean {
+    return _.isContained(pattern[1], tile, (ta, tb) => this.store.state.rule.compareTileByColor(ta, tb) === 0)
   }
 
   public getUid ():number {
@@ -56,6 +80,7 @@ export default class SolvedHuleTileDisplay extends Vue {
   }
 
   get solvedHuleTiles ():[IMianzi[], MahjongTile[], waitForm, number][] {
+    this.turnOffHuleTileFilter()
     return this.store.state.solvedHuleTile
   }
 
