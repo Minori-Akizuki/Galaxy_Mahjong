53ba15c247f94e9951107293183a1ff0da11441e
Minori Akizuki <dreamnstd@gmail.com>
2022-05-03 21:20:28 +0900

    clear branch to deploy page

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { key, store } from './store/store'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-const app = createApp(App)
-app.use(store, key)
-app.mount('#app')
