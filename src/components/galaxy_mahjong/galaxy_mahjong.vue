24816506b0703e5faa97d0efec46ada4d62f1315
Minori Akizuki <dreamnstd@gmail.com>
2022-05-16 20:26:24 +0900

    ツイートボタンとリポジトリリンクの追加 (#12)
    * add tweet button
    
    * リポジトリへのリンクの追加

--- src/components/galaxy_mahjong/galaxy_mahjong.vue
+++ src/components/galaxy_mahjong/galaxy_mahjong.vue
@@ -2,7 +2,10 @@
   <div>
     <h1>銀河麻雀</h1>
     <p>- Galaxy Mahjong - (β)</p>
-    <p style="color:dodgerblue">Be Mahjalien you TOO!</p>
+    <p style="color:dodgerblue">
+      Be Mahjalien you TOO!
+      <a :href="tweetButtonUrl" target="_blank"><img src="@/assets/twitter.png" /></a>
+    </p>
     <div class="readme">
       <p>◆下の牌一覧をクリック(タップ)して手牌を追加できます</p>
       <p>◆上の牌をクリック(タップ)すると削除できます</p>
@@ -21,7 +24,8 @@
     <ul>
       <li>元ネタ: <a href="https://youtu.be/IDaKM7eU7zE" target="_blank">麻雀星人からの挑戦状!?銀河麻雀！</a></li>
       <li>牌画像: <a href="https://majandofu.com/mahjong-images" target="_blank">豆腐麻雀</a></li>
-      <li>ページ作者 : <a href="https://twitter.com/Real_analysis">じつかいせき</a></li>
+      <li>ページ作者: <a href="https://twitter.com/Real_analysis">じつかいせき</a></li>
+      <li>リポジトリ: <a href="https://github.com/Minori-Akizuki/Galaxy_Mahjong">こちら</a></li>
     </ul>
   </div>
 </template>
@@ -51,9 +55,22 @@ import SolvedHandView from './solved_hand_view.vue'
 })
 export default class GalaxyMahjong extends Vue {
   store!: Store<State>
+  tweetButtonUrl!:string
   created ():void {
     this.store = useStore()
     this.store.commit(MUTATION.setMahjongRule, GalaxyMahjongRule.getInstance())
+    this.tweetButtonUrl = this.createTweetButtonUrl()
+  }
+
+  public createTweetButtonUrl () {
+    const params = {
+      text: '銀河麻雀 -君も麻雀星人になろう！-',
+      url: 'https://minori-akizuki.github.io/Galaxy_Mahjong/',
+      hashtags: '銀河麻雀'
+    }
+    const url = 'https://twitter.com/intent/tweet?'
+    const urlParam = new URLSearchParams(params).toString()
+    return url + urlParam
   }
 }
 </script>
